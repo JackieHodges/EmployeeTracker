f3fbd103f952f453ee35a12efb324abe0143a6d3
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-03-23 13:05:18 -0500

    added seed data

--- company_db.sql
+++ company_db.sql
@@ -12,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE role (
 	id INT NOT NULL AUTO_INCREMENT, 
 	title VARCHAR (30) NOT NULL,
-    salary DECIMAL NOT NULL,
+    salary INT NOT NULL,
     department_id INT NOT NULL,
     INDEX department_ind (department_id),
     FOREIGN KEY (department_id)
@@ -33,3 +33,23 @@ CREATE TABLE employee (
     manager_id INT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO department (name) VALUES 
+("Human Resources"),
+("IT"),
+("Accounting");
+
+INSERT INTO role (title, salary, department_id) VALUES
+("HR Buisness Partner", 100000, 1),
+("HR Associate", 70000, 1),
+("HR Intern", 50000, 1),
+("Product Manger", 150000, 2),
+("Senior Developer", 110000, 2),
+("Junior Developer", 85000, 2),
+("Senior Accountant", 110000, 3),
+("Accountant", 90000, 3),
+("Accounting Intern", 65000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+("Jackie", "Pederson", 6, 2),
+("Bryce", "Pederson", 7, 3);
