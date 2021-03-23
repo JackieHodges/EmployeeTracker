8e6c5fad190ba4d06be5f9ecd35ff1d15910fd1f
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-03-23 12:34:28 -0500

    added schemas to create db and tables

---
+++ company_db.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+	id INT NOT NULL AUTO_INCREMENT, 
+	name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+	id INT NOT NULL AUTO_INCREMENT, 
+	title VARCHAR (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    INDEX department_ind (department_id),
+    FOREIGN KEY (department_id)
+		REFERENCES department(id)
+        ON DELETE CASCADE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT NOT NULL,
+    INDEX role_ind (role_id),
+	FOREIGN KEY (role_id)
+		REFERENCES role(id)
+        ON DELETE CASCADE,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
