758b95127fb8127c9fc9c1f9062aec699c9f1a4c
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-03-23 13:24:59 -0500

    added initial inquirer prompts

--- index.js
+++ index.js
@@ -2,21 +2,55 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+    host: 'localhost',
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: 'root',
+    // Your username
+    user: 'root',
 
-  // Your password
-  password: process.env.PASSWORD,
-  database: 'company_db',
+    // Your password
+    password: process.env.PASSWORD,
+    database: 'company_db',
 });
 
 connection.connect((err) => {
-  if (err) throw err;
-  console.log("Successful connection");
-  connection.end();
+    if (err) throw err;
+    console.log("Successful connection");
+    start();
 });
+
+const start = () => {
+    inquirer
+        .prompt({
+            name: 'wantTo',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ["View All Employees", "View All Employees by Department", "Add Employee", "Add Department", "Add Role", "Update Employee Role", "Exit"],
+        })
+        .then((answer) => {
+            switch (answer.wantTo) {
+                case "View All Employees":
+                    console.log("See all employees selected");
+                    break;
+                case "View All Employees by Department":
+                    console.log("See all employees by department");
+                    break;
+                case "Add Employee":
+                    console.log("Add Employee selected");
+                    break;
+                case "Add Department":
+                    console.log("Add Department selected");
+                    break;
+                case "Add Role":
+                    console.log("Add Role selected");
+                    break;
+                case "Update Employee Role":
+                    console.log("Update employee role selected");
+                    break;
+                case "Exit":
+                    connection.end();
+            }
+        });
+};
